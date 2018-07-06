28f5e0f3bd804d08ab4dba9ac31336e13b9bdd78
Chris Kalahiki <chris.kalahiki@gmail.com>
2018-07-06 08:48:55 -0500

    Changed location of save file for data

--- src/text_mining.py
+++ src/text_mining.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #Read data into an array
-tweets_data_path = '../data/twitter_data.txt'
+tweets_data_path = 'twitter_data.txt'
 
 tweets_data = []
 tweets_file = open(tweets_data_path, "r")
